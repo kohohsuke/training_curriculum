bd92f199c99f081d2069ddb1dd0d428c30c49c91
kohohsuke <82505121+kohohsuke@users.noreply.github.com>
2021-04-27 17:00:06 +0900

    .to_iメソッドの追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,9 +36,9 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = wdays[(@todays_date + x).wday]
-       if wday_num >= 7
-         wday_num = wday_num -7
-       end
+      if wday_num.to_i >= 7
+        wday_num = wday_num -7
+      end
 
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
 
